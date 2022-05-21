20b483b80e0a9bf602fdf190ab8ffd05b41ba759
Ivan Peon <ivan.peon@leica-geosystems.com>
2022-05-21 15:05:15 +0200

    finalize event loop

--- contracts/eth_usdc.py
+++ contracts/eth_usdc.py
@@ -3,5 +3,5 @@ import contracts.eth_usdc_abi as abi
 
 address = '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc'
 
-token0 = [{"tracker": "usdc", "unit" : "mwei"}]
-token1 = [{"tracker": "eth", "unit" : "ether"}]
+token0 = {"tracker": "usdc", "unit": "mwei"}
+token1 = {"tracker": "eth", "unit": "ether"}
--- dex_events.py
+++ dex_events.py
@@ -6,16 +6,30 @@ import contracts.doe_token_abi as doe_token_abi
 import contracts.eth_usdc_abi as eth_usdc_abi
 import contracts.eth_usdc as eth_usdc
 
+address = eth_usdc.address
+token0 = eth_usdc.token0
+token1 = eth_usdc.token1
+
+def to_string(amount, token):
+    value = Web3.fromWei(amount, token['unit'])
+    return f'{value:,.4f} {token["tracker"]}'
+
 def handle_event(event):
     event_dict = json.loads(Web3.toJSON(event))
-    print(json.dumps(event_dict, indent=3))
-    print(f'Tx: {event_dict["transactionHash"]}')
-    Web3.fromWei(event.args.amount0In, 'ether')
-    print(f'In {eth_usdc.token0["tracker"]}  : {Web3.fromWei(event.args.amount0In,  eth_usdc.token0["unit"])}')
-    print(f'In {eth_usdc.token1["tracker"]} : {Web3.fromWei(event.args.amount1In,  eth_usdc.token1["unit"])}')
-    print(f'Out {eth_usdc.token0["tracker"]} : {Web3.fromWei(event.args.amount0Out,  eth_usdc.token0["unit"])}')
-    print(f'Out {eth_usdc.token1["tracker"]}: {Web3.fromWei(event.args.amount1Out, eth_usdc.token1["unit"])}')
-    # exit()
+    amountIn = event.args.amount0In
+    tokenIn = token0
+    amountOut = event.args.amount1Out
+    tokenOut = token1
+    if amountIn == 0:
+        amountIn = event.args.amount1In
+        tokenIn = token1
+        amountOut = event.args.amount0Out
+        tokenOut = token0
+    
+    print(f'Tx: https://etherscan.io/tx/{event_dict["transactionHash"]}')
+    print(f'In:  {to_string(amountIn, tokenIn)}')
+    print(f'Out: {to_string(amountOut, tokenOut)}')
+    exit()
 
 def log_loop(event_filter, poll_interval):
     while True:
@@ -25,10 +39,8 @@ def log_loop(event_filter, poll_interval):
 
 def main():
     w3_eth = Web3(Web3.HTTPProvider(hidden_details.eth_mainnet))
-    block_filter = w3_eth.eth.filter('latest')
-    contract_address = eth_usdc.address
-    contract = w3_eth.eth.contract(address=eth_usdc.address, abi=eth_usdc_abi.get_abi())
-    events_filter = contract.events.Swap.createFilter(fromBlock='latest') # 14787280
+    contract = w3_eth.eth.contract(address=address, abi=eth_usdc_abi.get_abi())
+    events_filter = contract.events.Swap.createFilter(fromBlock='latest')
     log_loop(events_filter, 2)
 
 if __name__ == '__main__':
